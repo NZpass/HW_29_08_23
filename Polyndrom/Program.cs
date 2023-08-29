f7d9a9381c21ac1914deaff4c0fd1de85bbeb61a
GJ <GJ@dog.su>
2023-08-29 11:23:14 +0300

    Полиндром

---
+++ Polyndrom/Program.cs
@@ -0,0 +1,27 @@
+﻿// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+// Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+// Для остальных чисел вернуть True или False.
+// 14212 -> False
+// 12821 -> True
+// 234322 -> Число не пятизначное
+
+Console.WriteLine ("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int plnd = number.Length;
+
+if (plnd == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine(number + " Это число палиндромом");
+    }
+
+    else
+    {
+        Console.WriteLine(number + " Это число не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы ввели не пятизначное число!");
+}
